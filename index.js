1f0b0459dd96a2e5d47e11771809707ed424dcad
Your Name <you@example.com>
2021-11-18 21:39:56 -0300

    Refatoração de código

--- index.js
+++ index.js
@@ -30,14 +30,145 @@ button.addEventListener('click', function(){
     java.style.backgroundColor = 'grey';
     button.style.backgroundColor = 'grey';
 
-    let titulo = document.createElement('h2');
-    titulo.innerText = 'Titulo'
-    titulo.className = 'titulo-modal'
 
-    let nome = document.createElement('input')
-    nome.className = 'input-name'
-    nome.placeholder = 'Insira seu nome'
-    
+    let titulo = document.createElement('h2');
+    titulo.innerText = 'CADASTRÃO';
+    titulo.className = 'titulo-modal';
+
+
     modal.appendChild(titulo);
+
+
+    let divNome = document.createElement('div');
+    divNome.className = 'div-nome';
+    
+
+    let nomeInput = document.createElement('input');
+    nomeInput.className = 'input-nome';
+    nomeInput.placeholder = 'Insira seu nome';;
+    
+
+    divNome.appendChild(nomeInput);
+    modal.appendChild(divNome);
+
+
+    let divSobrenome = document.createElement('div');
+    divSobrenome.className = 'div-nome';
+    
+    let sobrenomeInput = document.createElement('input');
+    sobrenomeInput.className = 'input-sobrenome';
+    sobrenomeInput.placeholder = 'Insira seu sobrenome';
+    
+    divSobrenome.appendChild(sobrenomeInput);
+    modal.appendChild(divSobrenome);
+
+
+    let divNasc = document.createElement('div');
+    divNasc.className = 'div-nasc';
+    
+    let nascInput = document.createElement('input');
+    nascInput.className = 'input-nasc';
+    nascInput.placeholder = 'Insira seu nascimento';
+    nascInput.type = 'date'
+    
+    divNasc.appendChild(nascInput);
+    modal.appendChild(divNasc);
+
+    let divBoToesInferior = document.createElement('div');
+    divBoToesInferior.className = 'div-botoes-inferior';
+
+
+    let enviar = document.createElement('button');
+    enviar.className = 'botao-enviar';
+    enviar.innerText = 'Cadastrar';
+
+    modal.appendChild(enviar)
+
+    divBoToesInferior.appendChild(enviar);
+
+    let cancelar = document.createElement('button');
+    cancelar.className = 'botao-cancelar';
+    cancelar.innerText = 'Cancelar';
+    
+    divBoToesInferior.appendChild(cancelar);
+
+    
+    enviar.onclick = function(){
+        if(nomeInput.value == '' || sobrenomeInput.value == '' || nascInput.value == ''){
+            
+            const vermelha = document.createElement('div');
+            vermelha.className = 'div-vermelha'
+            vermelha.innerHTML = '<p>HÁ ALGUM CAMPO VAZIO</p>';
+            vermelha.style.position = 'absolute';
+            vermelha.style.bottom = 0;
+            java.appendChild(vermelha);
+        }
+        
+        else if(nomeInput.value != '' && sobrenomeInput.value != '' && nascInput.value != ''){
+            const verde = document.createElement('div');
+            verde.className = 'div-verde'
+            verde.innerHTML = '<p>CADASTRO CONCLUÍDO</p>';
+            verde.style.position = 'absolute';
+            verde.style.bottom = 0;
+            
+            java.appendChild(verde);
+            
+            let intervalo = setTimeout(function(){
+                containerModal.remove();
+                verde.remove();
+                vermelha.remove()
+                
+                java.style.backgroundColor = 'transparent';
+                button.style.background = '#EFEFEF';
+                button.style.outline = 'none';
+            },3000)
+
+            
+            let nome = document.querySelector('.input-nome')
+            nomeValor = nome.value;
+            console.log(nomeValor)
+
+
+            let sobreNome = document.querySelector('.input-sobrenome')
+            sobreNomeValor = sobreNome.value;
+            console.log(sobreNomeValor)
+
+
+            let nascimento = document.querySelector('.input-nasc')
+            nascimentoValor = nascimento.value;
+            console.log(nascimentoValor)
+
+            let pessoa = {
+                nome: nomeValor,
+                sobrenome: sobreNomeValor,
+                nascimento: nascimentoValor
+            }
+
+            let valoresArr = [pessoa];
+
+
+            
+        }
+        
+        
+        
+        const cadastroArray = [];
+        
+        cadastroArray.push(nomeInput.value)
+        
+        cadastroArray.push(sobrenomeInput.value)
+        
+        cadastroArray.push(nascInput.value)
+    }
+    
+    cancelar.onclick = function(){
+        containerModal.remove();
+        java.style.backgroundColor = 'transparent';
+        button.style.background = '#EFEFEF';
+        button.style.outline = 'none';
+    }
+    
+    modal.appendChild(divBoToesInferior)
+    
 })
 
